1840fd7a6902c06c510d1000abdcad0bf724e05d
KuraiTsukino <sandym3000@hotmail.com>
2021-12-08 18:04:18 -0600

    prueba 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+// Importaciones
 import './App.css';
+import React, { useState } from "react"
 
 function App() {
+ 
+  const [comment, setComment] = useState({
+    title: "",
+    description: ""
+  })
+
+  const [commentList, setCommentList] = useState([])
+  /*
+  const generateMessage = () => {
+    return console.log("Hola mundo")
+  }
+  */
+
+  const enterData = (e) => {
+    console.log(e)
+    console.log(e.target.value)
+  
+
+  return setComment({
+    ...comment,
+    [e.target.name]: e.target.value
+  })
+}
+  //Create and read
+
+  const addComment = (e) => {
+    e.preventDefault()
+
+    setCommentList([
+      ...commentList,
+      comment
+    ])
+
+    setComment({
+      title: "",
+      description: ""
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Write a comment</h1>
+      <form onSubmit={(e) => {addComment(e)}}>
+        <h3>Write a title</h3>
+          <input 
+            name="title"
+            value={comment.title}
+            onChange={(e) => { enterData(e) }}
+          />
+        <h3>Write a description</h3>
+          <input 
+            name= "description"
+            value= {comment.description}
+            onChange={(e) => { enterData(e) }}
+          />
+        <button>Write comment</button>
+      </form>
+
+      <h2>Comment List</h2>
+      {
+        commentList.map((elem) => {
+          return(
+            <>
+              <h4>{elem.title}</h4>
+              <p>{elem.description}</p>
+            </>
+          )
+        })
+      }
+
+    </>
+  )
+
 }
 
 export default App;
